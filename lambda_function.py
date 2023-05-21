d7141b202edf77101ffbe515f17a954996aff86a
Aarón Fas <yo@aaron.com.es>
2023-05-21 21:09:19 +0200

    Package structure refactor. Filter some recurrent events.
    - Removed `acalendar` sub-module since it was still generating issues
      and made testing difficult.
    - Renamed `calendar.py`, `email.py` and `template.py` to less generic
      names with a `my_` prefix.
    - Adapt `lambra_function.py` to new structure.
    - Filter out daily and weekly events - not interested in those.

--- lambda_function.py
+++ lambda_function.py
@@ -3,13 +3,15 @@ import json
 import boto3
 from botocore.exceptions import ClientError
 from config import CALENDAR_ICS_URL, RECIPIENT, SENDER, DAYS
-from acalendar import email, template, calendar
+import my_email
+import my_template
+import my_calendar
 
 def lambda_handler(event, context):
     # Main program
-    events_per_day = calendar.get_events(CALENDAR_ICS_URL, DAYS)
-    body = template.generate(events_per_day)
-    email.send_email(SENDER, RECIPIENT, body)
+    events_per_day = my_calendar.get_events(CALENDAR_ICS_URL, DAYS)
+    body = my_template.generate(events_per_day)
+    my_email.send_email(SENDER, RECIPIENT, body)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
--- my_calendar.py
+++ my_calendar.py
@@ -14,7 +14,7 @@ def get_events(ics_url, days):
 
     # Using `recurring_ical_events` so that recurring events are correctly parsed.
     events = []
-    for component in recurring_ical_events.of(gcal).between(present, present + d):
+    for component in recurring_ical_events.of(gcal, keep_recurrence_attributes=True).between(present, present + d):
         if component.name == "VEVENT":
             dtstart = component.get('dtstart').dt
             dtend = None
@@ -32,6 +32,17 @@ def get_events(ics_url, days):
 
             if dtend != None and dtend >= present:
                 if dtstart >= present and dtstart <= present + d:
+                    # Skip some recurrent events
+                    if 'rrule' in component:
+                        frequency = component.get('rrule').get('freq')
+                        # Skip daily events - we are not interested in them.
+                        if 'DAILY' in frequency:
+                            continue
+                        # Skip weekly events (and only _purely_ weekly events)
+                        if 'WEEKLY' in frequency:
+                            # But not if they are not really weekly (bi-weekly or so)
+                            if 'interval' not in component.get('rrule'):
+                                continue
                     events.append({
                         "summary": component.get('summary'),
                         "start_date": dtstart,
@@ -50,5 +61,5 @@ def get_events(ics_url, days):
 
 if __name__ == "__main__":
     from config import CALENDAR_ICS_URL, DAYS
-    import pprint
+    from pprint import pprint
     pprint(get_events(CALENDAR_ICS_URL, DAYS))
